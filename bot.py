44610c9a4a5c864018042e511b1a90fd0eab389f
Baudoin Mathieu <baudoin.m@outlook.fr>
2020-11-18 20:35:27 +0100

    make it better

---
+++ bot.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix = "*", description = "lol")
+
+@bot.event
+async def on_ready():
+	print("Ready !")
+
+@bot.command()
+async def EDT(ctx):
+	await ctx.send(file=discord.File('Capture.png'))
+
+@bot.command()
+async def ryan(ctx):
+	await ctx.send("miskin boloss")
+
+bot.run("Nzc3OTcwMzg3NDEyNTgyNDEw.X7LLYg.98wsHtneuBWUgEtmr43MXqol5-U")
+
+
